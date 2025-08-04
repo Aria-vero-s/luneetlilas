b17ed4b0e39a2502901e3969d92cea1eb646b548
aria-vero-s <ariane.saulnier@gmail.com>
2025-08-04 16:08:41 +0200

    Fix root URL routing

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),  # everything in store.urls
+
+    # 1) Home page at /
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+
+    # 2) Everything under /store/
+    path('store/', include('store.urls')),
 ]
